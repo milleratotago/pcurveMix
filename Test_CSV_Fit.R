91b94f020062f7ac56c7a072f2920c1c91accb87
RolfUlrichTuebingen <136806905+RolfUlrichTuebingen@users.noreply.github.com>
2026-03-26 16:11:25 +0100

    ROLF26March2026

--- Test_CSV_Fit.R
+++ Test_CSV_Fit.R
@@ -1,12 +1,12 @@
 #Test file 
 
-set.seed(88)
+set.seed(8888)
 
-alpha_cutoff <- 0.05
-
-ps <- pcurveMix::random(n = 500, mu = 2, sigma = 2, pi = 0.5)
+ps <- pcurveMix::random(n = 10000, mu = 2, sigma = 2, pi = 0.5)
 
 ps[ps <= 0 | ps >= 1] # check if values are outside 0-1
+
+ps[ps==0] <- 1e-40    # no zeros
 
 ps_out <- format(ps, scientific = FALSE, trim = TRUE, decimal.mark = ".")
 
@@ -17,7 +17,13 @@ write.table(data.frame(p = ps_out),
             col.names = TRUE,
             quote = FALSE)
 
-fit<-pcurveMix::fit_p_curve(ps_out)
+alpha_cuff <- 1
 
-pcurveMix::fit_to_estimates_tbl(fit) # Estimates? 
+fit<-pcurveMix::fit_p_curve(ps_out,alpha=alpha_cuff)
+                            
+pcurveMix::fit_to_estimates_tbl(fit) 
+
+sum(ps_out < alpha_cuff, na.rm=TRUE)
+
+packageVersion("pcurveMix")
 
